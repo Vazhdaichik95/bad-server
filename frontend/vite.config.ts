3b5d385971d581fe5ba5260eac37af00b97d46a9
Vazhdaichik95 <vazhdaichik95@yandex.ru>
2026-06-14 20:38:17 +0300

    audit2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,29 @@
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import svgr from "vite-plugin-svgr";
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ svgr(), react(), tsconfigPaths({root: __dirname})],
+  plugins: [svgr(), react()],
   resolve: {
     alias: {
-      $fonts: resolve('./src/vendor/fonts'),
-      $assets: resolve('./src/assets'),
-    }
+      '@': resolve(__dirname, './src'),
+      $fonts: resolve(__dirname, './src/vendor/fonts'),
+      $assets: resolve(__dirname, './src/assets'),
+    },
+    tsconfigPaths: true,
   },
   build: {
-    assetsInlineLimit:0,
+    assetsInlineLimit: 0,
   },
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @use "./src/scss/variables" as *;
-          @use "./src/scss/mixins";
+          @use "@/scss/variables" as *;
+          @use "@/scss/mixins" as mixins;
         `,
       },
-
-    }
+    },
   },
-
 })
