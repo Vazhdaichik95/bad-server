ec140c912bf1ece8d1478967e31464187c2be4c6
Vazhdaichik95 <vazhdaichik95@yandex.ru>
2026-06-23 23:10:09 +0300

    fix 10

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,7 +28,7 @@ const corsOptions = {
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 20,
+    max: 40,
     standardHeaders: true,
     legacyHeaders: false,
     message: {
